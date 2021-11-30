7fa09c9d857ddc3b1f3e3cae54bf8da5650b773d
ibrahimakpinar <ibrahim.akpinar@outlook.com.tr>
2021-12-01 00:30:10 +0300

    added firebase phone authentication

--- SimpleChatApp/SimpleChatApp/SignIn/Controllers/VerifyPhoneNumberViewController.swift
+++ SimpleChatApp/SimpleChatApp/SignIn/Controllers/VerifyPhoneNumberViewController.swift
@@ -7,25 +7,56 @@
 
 import UIKit
 import SimpleChatAppAPI
+import Firebase
+// Swift
+import FirebasePhoneAuthUI
 
-class VerifyPhoneNumberViewController: UIViewController, Storyboarded {
+class VerifyPhoneNumberViewController: UIViewController, Storyboarded, FUIAuthDelegate {
     @IBOutlet weak var textCountryCode: UITextField!
     @IBOutlet weak var textPhoneNumber: UITextField!
+    @IBOutlet weak var verifyButton: UIButton!
     var viewModel: VerifyPhoneNumberViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = viewModel?.getTitle()
-        self.textCountryCode.addBottomBorder()
-        self.textPhoneNumber.addBottomBorder()
-        
+        self.verifyButton.isEnabled = false
         self.textCountryCode.addTarget(self, action: #selector(countryCodeOnTapped),
                                        for: .touchDown)
     }
     
+    @IBAction func verifyButtonTapped(_ sender: Any) {
+        
+        guard let completeNumber = viewModel?.completeNumber else {
+           return
+        }
+        
+        PhoneAuthProvider.provider()
+          .verifyPhoneNumber(completeNumber, uiDelegate: nil) { verificationID, error in
+              if let error = error {
+                print(error.localizedDescription)
+                return
+              }
+            
+              UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
+        }
+    }
+    
+    
     @objc func countryCodeOnTapped(_ sender: Any) {
         viewModel?.openCountryCodeListView(withSelf: self)
     }
+    
+    @IBAction func phoneNumberEditingChanged(_ sender: Any) {
+        if let textField = sender as? UITextField {
+            viewModel?.phoneNumber = textField.text
+            
+            if viewModel?.completeNumber != nil {
+                self.verifyButton.isEnabled = true
+            }
+        }
+    }
+    
 }
 
 
@@ -33,6 +64,13 @@ class VerifyPhoneNumberViewController: UIViewController, Storyboarded {
 extension VerifyPhoneNumberViewController: CountryCodeListCoordinatorDelegate {
     func didSelect(countryCode: CountryCode) {
         self.navigationController?.popViewController(animated: true)
-        self.textCountryCode.text = countryCode.dialCode
+        self.textCountryCode.text = "\(countryCode.dialCode) (\(countryCode.name))"
+        self.viewModel?.dialCode = countryCode.dialCode
+        /*
+        FUIAuth.defaultAuthUI()?.delegate = self
+        let phoneProvider = FUIPhoneAuth.init(authUI: FUIAuth.defaultAuthUI()!)
+        FUIAuth.defaultAuthUI()?.providers = [phoneProvider]
+        phoneProvider.signIn(withPresenting: self, phoneNumber: nil)
+        */
     }
 }
--- SimpleChatApp/SimpleChatApp/SignIn/ViewModel/VerifyPhoneNumberViewModel.swift
+++ SimpleChatApp/SimpleChatApp/SignIn/ViewModel/VerifyPhoneNumberViewModel.swift
@@ -19,6 +19,14 @@ protocol VerifyPhoneNumberViewModelProtocol {
 }
 
 final class VerifyPhoneNumberViewModel: VerifyPhoneNumberViewModelProtocol {
+    var dialCode: String?
+    var phoneNumber: String?
+    
+    var completeNumber: String? {
+        let number = "\(String(describing: dialCode ?? ""))\(String(describing: phoneNumber ?? ""))"
+        return number.isValidPhoneNumber() ? number : nil
+    }
+    
     func didCountryCodeSelect(countryCode: CountryCode) {
         
     }
@@ -35,5 +43,13 @@ final class VerifyPhoneNumberViewModel: VerifyPhoneNumberViewModelProtocol {
     
     func openCountryCodeListView(withSelf vcSelf: VerifyPhoneNumberViewController){
         coordinatorDelegate?.openCountryCodeListView(withSelf: vcSelf)
+    }
+}
+
+extension String {
+    func isValidPhoneNumber() -> Bool {
+        let regEx = "^\\+(?:[0-9] ?){6,14}[0-9]$"
+        let phoneCheck = NSPredicate(format: "SELF MATCHES[c] %@", regEx)
+        return phoneCheck.evaluate(with: self)
     }
 }
